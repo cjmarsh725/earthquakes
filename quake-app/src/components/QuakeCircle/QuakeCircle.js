d68d6bfa6bdb4ae1670c66151ccbe0e09db2e425
Chris <cjmarsh725@gmail.com>
2019-01-02 16:24:28 -0800

    initiated quake circle component

---
+++ quake-app/src/components/QuakeCircle/QuakeCircle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './QuakeCircle.css';
+
+class QuakeCircle extends Component {
+  render() {
+    return (
+      <div className="circle-container">
+        QuakeCircle
+      </div>
+    );
+  }
+}
+
+export default QuakeCircle;
