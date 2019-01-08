8a66839bb7e83f0ce76b41e252e6308678c2135b
Chris <cjmarsh725@gmail.com>
2019-01-07 18:34:36 -0800

    initialized quake graph component

---
+++ quake-app/src/components/QuakeGraph/QuakeGraph.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './QuakeGraph.css';
+
+class QuakeGraph extends Component {
+  render() {
+    return (
+      <div className="graph-container">
+        QuakeGraph
+      </div>
+    );
+  }
+}
+
+export default QuakeGraph;
