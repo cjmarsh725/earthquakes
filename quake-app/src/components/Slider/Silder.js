86c121758921b45758464e5abfd2dd4728721f61
Chris <cjmarsh725@gmail.com>
2019-01-10 17:45:38 -0800

    initialized slider component

---
+++ quake-app/src/components/Slider/Silder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Slider.css';
+
+class Slider extends Component {
+  render() {
+    return (
+      <div className="slider-container">
+        Slider
+      </div>
+    );
+  }
+}
+
+export default Slider;
